6eca0f14ede5ef2f2f66272f1b1f527cad77a267
Cody Lee <CodyJLeeApps@icloud.com>
2017-10-03 11:58:08 -0400

    removing junk code from constants.h, adding example constant line as a reminder later on.

--- projects/linked_list/inc/constants.h
+++ projects/linked_list/inc/constants.h
@@ -2,8 +2,7 @@
 #define CONSTANTS_H
 #pragma once
 
-// BASEID Required, UUID REQUIRED, EXAMID optional
-static int const MIN_GOOD_ARG_LENGTH    = 3;
-static int const MIN_GOOD_UUEXAM_LENGTH = 5;
+// ***** Constants *****
+//static int const EXAMPLE_CONSTANT = 3;
 
-#endif // CONSTANTS_H
+#endif
